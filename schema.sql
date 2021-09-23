3522d53425c02727df123a1e9adc87cb224bb3e2
Ursula Wilborn <82011348+uwilborn@users.noreply.github.com>
2021-09-23 16:33:32 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -6,26 +6,26 @@ USE ecommerce_db;
 
 
 CREATE TABLE Category (
-    id integer null Primary key Auto_increment, 
-    category_name string null,
+    id integer NOt Null Primary key Auto_increment, 
+    category_name string NOT NULL
     );
 
 create table Product (
-    id integer null Primary key Auto_increment, 
-    product_name string null,
-    price decimal(10, 2) null,
-    stock integer null value(10)
-    category_id interger references (Category_id)
+    id integer NOT NULL Primary key Auto_increment, 
+    product_name string NOT NULL,
+    price decimal(10, 2) NOT NULL,
+    stock integer NOT NULL DEFAULT '10',
+    category_id integer references (Category_id)
     );
     
  create table Tag (
-    id integer null Primary key Auto_increment, 
-    tag_name string,
+    id integer NOT NULL Primary key Auto_increment, 
+    tag_name string
     
     );
     
  create table ProductTag (
-    id integer null Primary key Auto_increment, 
+    id integer NOT NULL Primary key Auto_increment, 
     product_id integer references (Product_id)
     tag_id integer references (Tag_id)
     
