4f35b912189ff53109f79b19806554e9e293d60a
Ursula Wilborn <82011348+uwilborn@users.noreply.github.com>
2021-09-23 17:07:48 -0400

    Update Product.js

--- Product.js
+++ Product.js
@@ -9,7 +9,30 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    {
+     id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  price: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+      stock: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+      category_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
   },
   {
     sequelize,
