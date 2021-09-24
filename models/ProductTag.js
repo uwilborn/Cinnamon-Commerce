f40774650fdef7e018d87d0eada2284a1d334254
Ursula Wilborn <82011348+uwilborn@users.noreply.github.com>
2021-09-24 15:51:27 -0400

    Update ProductTag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -14,11 +14,17 @@ ProductTag.init(
     },
     product_id: {
       type: DataTypes.INTEGER,
-      References Product model_id,
+      references: {
+        model: 'Product',
+        key: 'id',
+      },
     },
     tag_id: {
       type: DataTypes.INTEGER,
-      References Tag model_id,
+      references: {
+        model: 'Tag',
+        key: 'id',
+      },
     },
   {
     sequelize,
