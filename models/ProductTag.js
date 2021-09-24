7ea33c0039d342de07e38d2dad166db8a9464615
Ursula Wilborn <82011348+uwilborn@users.noreply.github.com>
2021-09-24 15:46:24 -0400

    Update ProductTag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,8 +6,20 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
-  },
+      id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      References Product model_id,
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      References Tag model_id,
+    },
   {
     sequelize,
     timestamps: false,
